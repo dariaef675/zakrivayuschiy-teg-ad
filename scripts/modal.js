50f32b3810697f5d528c64df1b57e620b6f43fd1
Daria Efimova <defimova710@gmail.com>
2026-08-10 17:38:02 +0300

    изменили шрифты и гажатие кнопок

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,16 +1,10 @@
 const modal = document.querySelector('.modal');
 const openModalButton = document.querySelector('.save-button');
-const closeModalButton = document.querySelector('.modal__button');
 
 openModalButton.addEventListener('click', (event) => {
   event.preventDefault();
   modal.showModal();
   modal.classList.add('opened');
-});
-
-closeModalButton.addEventListener('click', (event) => {
-  event.preventDefault();
-  modal.close();
 });
 
 modal.addEventListener('close', () => {
