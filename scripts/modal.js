db3653ad0e3055b66bbe935869114a8c9b3b10c8
Daria Efimova <defimova710@gmail.com>
2026-08-11 12:26:21 +0300

    modal.js

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,20 +1,16 @@
+
 const modal = document.querySelector('.modal');
 const openModalButton = document.querySelector('.save-button');
 const closeModalButton = document.querySelector('.modal__button');
 
-
-openModalButton.addEventListener('click', (event) => {
-  event.preventDefault();
+openModalButton.addEventListener('click', () => {
   modal.showModal();
   modal.classList.add('opened');
 });
 
-
-closeModalButton.addEventListener('click', (event) => {
-  event.preventDefault();
-  modal.close(); 
+closeModalButton.addEventListener('click', () => {
+  modal.close();
 });
-
 
 modal.addEventListener('close', () => {
   modal.classList.remove('opened');
