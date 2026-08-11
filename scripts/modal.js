763ccc82a9df130f756300f88e765aea00236aaf
Daria Efimova <defimova710@gmail.com>
2026-08-11 12:16:02 +0300

    добавлены другие обработчики в modal.js

--- scripts/modal.js
+++ scripts/modal.js
@@ -2,18 +2,19 @@ const modal = document.querySelector('.modal');
 const openModalButton = document.querySelector('.save-button');
 const closeModalButton = document.querySelector('.modal__button');
 
+
 openModalButton.addEventListener('click', (event) => {
   event.preventDefault();
-
   modal.showModal();
   modal.classList.add('opened');
 });
 
+
 closeModalButton.addEventListener('click', (event) => {
   event.preventDefault();
-
-  modal.close();
+  modal.close(); 
 });
+
 
 modal.addEventListener('close', () => {
   modal.classList.remove('opened');
