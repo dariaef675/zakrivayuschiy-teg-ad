28bb1a65aab4ceb0c35d7d2957882acc71fcfa81
Daria Efimova <defimova710@gmail.com>
2026-08-11 12:01:02 +0300

    изменила кнопки и убрала preventdefault

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,10 +1,14 @@
 const modal = document.querySelector('.modal');
 const openModalButton = document.querySelector('.save-button');
+const closeModalButton = document.querySelector('.modal__button');
 
-openModalButton.addEventListener('click', (event) => {
-  event.preventDefault();
+openModalButton.addEventListener('click', () => {
   modal.showModal();
   modal.classList.add('opened');
+});
+
+closeModalButton.addEventListener('click', () => {
+  modal.close();
 });
 
 modal.addEventListener('close', () => {
